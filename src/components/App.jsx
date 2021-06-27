c52bc3101e25af492484eb4ef80c81d8fa9917b8
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-06-27 18:43:36 -0500

    working on firebase auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Register from './Register'
 import { Container} from 'react-bootstrap'
+import { AuthProvider } from '../contexts/AuthContext';
 
 function App() {
 
   return (
-    <Container 
-      className="d-flex align-items-center justify-content-center" 
-      style={{ minHeight: "100vh"}}>
+    <AuthProvider>
+      <Container 
+        className="d-flex align-items-center justify-content-center" 
+        style={{ minHeight: "100vh"}}>
         <div className="w-100" style={{maxWidth: "400px"}}>
           <Register />
-        </div>
-      
-    </Container>
+        </div> 
+      </Container>
+    </AuthProvider>
+    
     
   );
 }
