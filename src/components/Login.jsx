5588af3e92deeef55570b62da0cdf39e05420745
Nic Babineaux <76629649+NicBab@users.noreply.github.com>
2021-06-27 22:35:32 -0500

    firebase signup/login/ routing is  GO

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,29 +1,26 @@
 import React, { useRef, useState }  from 'react'
+import { Link } from 'react-router-dom'
 import { Form, Card, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
  
-const Register = () => {
+const Login = () => {
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { signup, currentUser } = useAuth()
+    const { login, currentUser } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
 
     async function handleSubmit(e) {
       e.preventDefault()
 
-      if (passwordRef.current.value !== 
-        passwordConfirmRef.current.value) {
-          return setError('Passwords do not match')
-        }
+
         try {
           setError('')
           setLoading(true)
-          signup(emailRef.current.value, passwordRef.current.value)
+          await login(emailRef.current.value, passwordRef.current.value)
 
         } catch (error) {
-          console.error('Failed to create account!')
+          console.error('Failed to sign in!')
         }
       setLoading(false)
     }
@@ -32,7 +29,7 @@ const Register = () => {
         <>
           <Card>
               <Card.Body>
-                  <h2 className="text-center mb-4">Sign UP</h2>
+                  <h2 className="text-center mb-4">Log In</h2>
                   {currentUser.email}
                     {error && <Alert variant="danger">{error}</Alert>}
                   <Form onSubmit={handleSubmit}>
@@ -44,18 +41,16 @@ const Register = () => {
                         <Form.Label>password</Form.Label>
                         <Form.Control type="password" ref={passwordRef} required />
                       </Form.Group>
-                      <Form.Group id="password-confirm">
-                        <Form.Label>password confirmation</Form.Label>
-                        <Form.Control type="password" ref={passwordConfirmRef} required />
-                      </Form.Group>
-                      <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
+                      <Button disabled={loading} className="w-100" type="submit">Login</Button>
                   </Form>
               </Card.Body>
           </Card>
-          <div className="w-100 text-center mt-2"></div>
-          Already have an account? LOGIN
+          <div className="w-100 text-center mt-2">
+            Need an account? <Link to="/signup">Sign Up here</Link> 
+          </div>
+          
         </>
     )
 }
 
-export default Register
+export default Login 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Signup } from './Signup';
+
+export { default as Login } from './Login'
+
+export { default as Home } from './Home'
+
+export { default as Dashboard } from './Dashboard'
